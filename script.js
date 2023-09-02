ad1b3b12fc7e07c62ad4e713406d61eef0001cba
prathamesh <shelke.prathamesh0012@gmail.com>
2023-09-02 13:10:17 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+/*practicing primitives and references in js */
+
+
+/*primitives*/
+
+let a = 10;
+let b = a
+b = 12
+console.log(b)
+
+/* In primitives we can directly copy the vale of another variable */
+
+/*references */
+
+let c = [1,2,3,4,5]
+let d = c
+d.push(13)
+
+
+
 
 
 

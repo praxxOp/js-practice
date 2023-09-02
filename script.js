c8f19e7f9e7ece21b3852cedd74ab84d55ff1bf1
prathamesh <shelke.prathamesh0012@gmail.com>
2023-09-02 13:34:18 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -13,8 +13,28 @@ console.log(b)
 /*references */
 
 let c = [1,2,3,4,5]
-let d = c
+let d = [...c]
+/* to copy all value of array or object  we use spread [...] operators in js */
 d.push(13)
+
+let e = {
+    name: "praxx",
+    age : 20,
+}
+
+let f = {...e}
+f.hair="black"; /*to push the vale in object we use  . notation */
+
+
+/* rest operator */
+
+const praxx= (a,b,c,...d)=>{
+console.log(a , b , c , d)
+}
+
+praxx(1,2,3,4,5,6,7,8,9)
+/* we use rest operator to store rest values in single variable in the form of array */
+
 
 
 

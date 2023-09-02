9ecc09c4498437d76ad9292b83282c0f43a74e65
prathamesh <shelke.prathamesh0012@gmail.com>
2023-09-02 13:59:42 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -1,43 +1,47 @@
 /*practicing primitives and references in js */
 
-
 /*primitives*/
 
 let a = 10;
-let b = a
-b = 12
-console.log(b)
+let b = a;
+b = 12;
+console.log(b);
 
 /* In primitives we can directly copy the vale of another variable */
 
 /*references */
 
-let c = [1,2,3,4,5]
-let d = [...c]
+let c = [1, 2, 3, 4, 5];
+let d = [...c];
 /* to copy all value of array or object  we use spread [...] operators in js */
-d.push(13)
+d.push(13);
 
 let e = {
-    name: "praxx",
-    age : 20,
-}
+  name: "praxx",
+  age: 20,
+};
 
-let f = {...e}
-f.hair="black"; /*to push the vale in object we use  . notation */
-
+let f = { ...e };
+f.hair = "black"; /*to push the vale in object we use  . notation */
 
 /* rest operator */
 
-const praxx= (a,b,c,...d)=>{
-console.log(a , b , c , d)
-}
+const praxx = (a, b, c, ...d) => {
+  console.log(a, b, c, d);
+};
 
-praxx(1,2,3,4,5,6,7,8,9)
+praxx(1, 2, 3, 4, 5, 6, 7, 8, 9);
 /* we use rest operator to store rest values in single variable in the form of array */
 
+/*hoisting*/
 
+console.log(g);
+var g = 30;
 
-
-
+/* we can use hoisting only in var not in let or const */
+/* Hoisting with var:
+var variables are hoisted and initialized with undefined, making them accessible before declaration,
+ whereas let and const variables are hoisted but not initialized, leading to ReferenceErrors if accessed before declaration.
+*/
 
 

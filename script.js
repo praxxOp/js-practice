f3142b91d83a9ab06b25fb8a683aea0ec6ed2307
prathamesh <shelke.prathamesh0012@gmail.com>
2023-09-11 16:36:15 +0530

    modified:   script.js

--- script.js
+++ script.js
@@ -255,7 +255,7 @@ console.log(john.name); // "John"
 
 axios.get('https://jsonplaceholder.typicode.com/posts/1')
   .then((response) => {
-    console.log('Response data:', response.data);
+    console.log('Response data:', response.data );
   })
   .catch((error) => {
     console.error('Error:', error);
